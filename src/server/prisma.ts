58740f0a4c1be1e7371c2d210134121af4f38557
Ronny2k-git <ronnyedson12@gmail.com>
2025-12-27 14:38:52 -0300

    chore: update prisma datasource

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -1,8 +1,10 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client'
+import pkg from '@prisma/client'
+
+const { PrismaClient } = pkg
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: InstanceType<typeof PrismaClient> | undefined
 }
 
 export const prisma =
